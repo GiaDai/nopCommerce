ebee3ba386a17c03a1563459890fb6f40b4b4c6b
Gia Bao <soichien333@gmail.com>
2024-01-13 14:47:59 +0700

    Config initial services for NopApi

---
+++ src/Presentation/Nop.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Catalog;
+
+namespace Nop.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+        private readonly ICategoryService _categoryService;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger
+            , ICategoryService categoryService
+            )
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var homepageCategories = await _categoryService.GetAllCategoriesDisplayedOnHomepageAsync();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetCategory", Name = "GetCategory")]
+        public async Task<IActionResult> GetCategory()
+        {
+            var homepageCategories = await _categoryService.GetAllCategoriesDisplayedOnHomepageAsync(true);
+            return Ok(homepageCategories);
+        }
+    }
+}
